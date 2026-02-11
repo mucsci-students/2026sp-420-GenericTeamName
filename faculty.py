73c1eed3584c676e9ed0705d716ee3f54ad1cbe9
dacrawf1 <dacrawf1@millersville.edu>
2026-02-10 23:13:41 -0500

    This contains the file I will be using for faculty management. This is a test to make sure that the file is committed correctly.

---
+++ faculty.py
@@ -0,0 +1,10 @@
+# Damion Crawford 
+# CMSC 420 Spring 2026
+
+# Names
+
+# Positions (full-tine or adjunct; course limits will also be defined here)
+
+# Available Hours/Days
+
+# Course preferences
